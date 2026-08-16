da0bc278d01ed263e472918a13f06439e463ff1f
hackathon26gitforce-debug <hackathon26.git.force@gmail.com>
2026-08-17 01:12:04 +0300

    Add app and CI workflow

---
+++ app.js
@@ -0,0 +1,4 @@
+import { greet, DEFAULT_USER } from "./user.js";
+
+console.log(greet(DEFAULT_USER));
+console.log("App started OK");
---
+++ user.js
@@ -0,0 +1,5 @@
+export function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+export const DEFAULT_USER = "world";
